49a1dce184d42d2182551dbbb2c97ed29458e337
JorikdeBoer <j.de.boer.40@student.rug.nl>
2016-09-12 14:35:43 +0200

    Exercise 2.3  c++ course upload Github Test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,44 @@
-#include "my_functions.h"
-#include <iostream>
+/*	Excercise 2.3.cpp
+Written on 12-09-2016 by Jorik
+This program calculates the molecular weight of two different salts. */
 
-int main() {
-  std::cout << add(40,2) << '\n';
+#include <iostream>
+using namespace std;
+
+void terminateProgram()
+{
+    cout << "Press any character and <ENTER> to continue" << endl;
+    char chAnyChar;
+    cin >> chAnyChar;
+    return;
+}
+
+void writeOutput(double dWeight)
+{
+    cout << "The molecular weight is " << dWeight << " g/mol"
+        << endl;
+    return;
+}
+
+int main()
+{
+    // declaration of atomic weights
+    const double dWeightC = 12.011, dWeightH = 1.008,
+        dWeightN = 14.007, dWeightNa = 22.999,
+        dWeightO = 15.999, dWeightP = 30.973,
+        dWeightS = 32.065;
+    // calculation of molecular weight of
+    // trisodium salt of ATP (C10H13N5Na3O13P3)
+    double dWeighttrisodiumsaltofATP =
+        10 * dWeightC + 13 * dWeightH + 5 * dWeightN +
+        3 * dWeightNa + 13 * dWeightO + 3 * dWeightP;
+    writeOutput(dWeighttrisodiumsaltofATP);
+    // calculation of molecular weight of
+    // Sildenafil citrate (C28H38N6O11S)
+    double dWeightSildenafilCitrate =
+        28 * dWeightC + 38 * dWeightH + 6 * dWeightN +
+        11 * dWeightO + 1 * dWeightS;
+    writeOutput(dWeightSildenafilCitrate);
+    //terminateProgram();
+    terminateProgram();
 }
